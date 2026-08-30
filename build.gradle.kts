fe710d1e0046f07f262cb5ddf80dbcb39170ccf9
inbydev <a80e2517-1246-49a5-907a-c38b8c2c97fb.1yg40@silomails.com>
2026-08-30 15:39:51 -0400

    chore(build): Separated Functional and POO taks for gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,28 @@ repositories {
 }
 
 application {
-    mainClass.set("dev.patata.caso.MainKt")
+    mainClass.set("dev.patata.caso.FuncionalKt")
 }
 
 tasks.named<JavaExec>("run") {
+    description = "Ejecuta la aplicación por defecto"
+    standardInput = System.`in`
+}
+
+// Tarea específica para la versión Funcional
+tasks.register<JavaExec>("runFun") {
+    group = "application"
+    description = "Ejecuta la versión Funcional"
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("dev.patata.caso.FuncionalKt")
+    standardInput = System.`in`
+}
+
+// Tarea específica para la versión POO
+tasks.register<JavaExec>("runPoo") {
+    group = "application"
+    description = "Ejecuta la versión Orientada a Objetos (POO)"
+    classpath = sourceSets["main"].runtimeClasspath
+    mainClass.set("dev.patata.caso.poo.POOKt")
     standardInput = System.`in`
 }
